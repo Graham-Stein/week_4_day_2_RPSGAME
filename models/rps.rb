ce4ca53e09d08680f2d7e34b1068b61eda3309ef
Graham-Stein <graham@guavoaviation.co.uk>
2018-10-02 17:19:24 +0100

    Extensions complete, if in need of some serious refactoring

--- models/rps.rb
+++ models/rps.rb
@@ -7,18 +7,17 @@ class RPS
     if player_1 == player_2
         return "#{player_1.capitalize} ties #{player_2.capitalize}!"
       elsif player_1 == 'rock' && player_2 == 'scissors'
-        return "#{player_1.capitalize} beats #{player_2.capitalize}!"
+        return "Player 1 wins, #{player_1.capitalize} beats #{player_2.capitalize}!"
       elsif player_1 == 'scissors' && player_2 == 'paper'
-        return "#{player_1.capitalize} beats #{player_2.capitalize}!"
+        return "Player 1 wins, #{player_1.capitalize} beats #{player_2.capitalize}!"
       elsif player_1 == 'paper' && player_2 == 'rock'
-        return "#{player_1.capitalize} beats #{player_2.capitalize}!"\
-
+        return "Player 1 wins, #{player_1.capitalize} beats #{player_2.capitalize}!"
       elsif player_2 == 'rock' && player_1 == 'scissors'
-        return "#{player_2.capitalize} beats #{player_1.capitalize}!"
+        return "Player 2 wins, #{player_2.capitalize} beats #{player_1.capitalize}!"
       elsif player_2 == 'scissors' && player_1 == 'paper'
-        return "#{player_2.capitalize} beats #{player_1.capitalize}!"
+        return "Player 2 wins, #{player_2.capitalize} beats #{player_1.capitalize}!"
       elsif player_2 == 'paper' && player_1 == 'rock'
-        return "#{player_2.capitalize} beats #{player_1.capitalize}!"
+        return "Player 2 wins, #{player_2.capitalize} beats #{player_1.capitalize}!"
       else
         return "Invalid fight"
     end # if end
--- specs/rps_spec.rb
+++ specs/rps_spec.rb
@@ -6,15 +6,17 @@ require('minitest/rg')
 class TestRPS < MiniTest::Test
 
   def test_player_1_victory
-    assert_equal("Rock beats Scissors!", RPS.shoot('rock', 'scissors'))
+    assert_equal("Player 1 wins, Rock beats Scissors!", RPS.shoot('rock', 'scissors'))
   end
 
   def test_player_1_loss
-    assert_equal("Paper beats Rock!", RPS.shoot('paper', 'rock'))
+    assert_equal("Player 2 wins, Paper beats Rock!", RPS.shoot('rock', 'paper'))
   end
 
   def test_tie
     assert_equal("Rock ties Rock!", RPS.shoot('rock', 'rock'))
   end
+
+
 
 end
