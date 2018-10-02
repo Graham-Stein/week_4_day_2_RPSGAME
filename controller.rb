33af2aabcc27e2a041d3a6d7fb938fe978311ab4
Thomas Gracie <tgracie030@gmail.com>
2018-10-02 16:06:31 +0100

    MVP, to add - which player is which

--- controller.rb
+++ controller.rb
@@ -3,3 +3,11 @@ require('sinatra/contrib/all')
 require('pry')
 require_relative('./models/rps.rb')
 also_reload('./models/*')
+
+get '/rps/:player_1/:player_2' do
+  player_1 = params[:player_1]
+  player_2 = params[:player_2]
+  @rps = RPS.shoot(player_1, player_2)
+  # binding.pry
+  erb(:result)
+end
--- models/rps.rb
+++ models/rps.rb
@@ -11,7 +11,14 @@ class RPS
       elsif player_1 == 'scissors' && player_2 == 'paper'
         return "#{player_1.capitalize} beats #{player_2.capitalize}!"
       elsif player_1 == 'paper' && player_2 == 'rock'
-        return "#{player_1.capitalize} beats #{player_2.capitalize}!"
+        return "#{player_1.capitalize} beats #{player_2.capitalize}!"\
+
+      elsif player_2 == 'rock' && player_1 == 'scissors'
+        return "#{player_2.capitalize} beats #{player_1.capitalize}!"
+      elsif player_2 == 'scissors' && player_1 == 'paper'
+        return "#{player_2.capitalize} beats #{player_1.capitalize}!"
+      elsif player_2 == 'paper' && player_1 == 'rock'
+        return "#{player_2.capitalize} beats #{player_1.capitalize}!"
       else
         return "Invalid fight"
     end # if end
